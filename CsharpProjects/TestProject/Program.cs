b1e3993c20d76cac4a681cf9861d12a5e6f99dcf
3005Haruna1997 <kirya-ivanoff-kirill@mail.ru>
2024-06-04 14:23:36 +0500

    Initial commit

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задание_1
+Console.WriteLine("Hello world !!!");
+Console.ReadKey();
+// Задание_2
+Console.Write("Hello");
+Console.Write(" ");
+Console.Write("world"+" "+"!!!");
+Console.ReadLine();
---
+++ README.md
@@ -0,0 +1,2 @@
+# work_1
+ skillboxWork_1
